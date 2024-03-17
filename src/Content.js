f44e5b6b10ddf940a09611659156022fe34a6aa5
BilalZafar99598 <bilal.halfur@gmail.com>
2024-03-18 00:40:17 +0500

    Content Component Added

--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,39 @@
+import { useState } from 'react';
+
 const Content = () => {
+    const [name, setName] = useState('Bilal');
+    const [count, setCount] = useState(0);
+
     const handleNameChange = () => {
         const names = ['Bob', 'Kevin', 'Dave'];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        // return names[int];
+        setName(names[int]);
+    }
+
+    const handleClick = () => {
+        setCount(count+1);
+        setCount(count+1);
+        console.log(count);
+    }
+
+    const onHandleChecked = (name) => {
+        return console.log(`Name ${name} was entered!`);
+    }
+
+    const onHandle3= () => {
+        console.log(count);
     }
 
     return (
         <main>
             <p>
-                Hello {handleNameChange()}!
+                Hello {name}!
             </p>
+            <button onClick={handleNameChange}>Change Name</button>
+            <button onClick={handleClick}>Count Click</button>
+            <button onClick={() => onHandleChecked('Omer')}>Click 2</button>
+            <button onClick={onHandle3}>Click 3</button>
         </main>
     )
 }
